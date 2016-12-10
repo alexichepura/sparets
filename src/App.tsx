744650824b936568e862b65995c0af908d3d2752
Alexi Chepura <a.chepura@gmail.com>
2016-12-10 03:09:22 +0300

    react-router, ts 2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import Header from './part/Header'
-import Counter from './part/Counter'
+import Home from './page/Home'
+import Login from './page/Login'
+import Counter from './page/Counter'
+import Router from 'react-router/BrowserRouter'
+import Match from 'react-router/Match'
+import Link from 'react-router/Link'
 
 export default class App extends React.Component<any, any> {
   constructor(props: any) {
@@ -8,11 +13,22 @@ export default class App extends React.Component<any, any> {
   }
 
   render() {
-    return <div>
-      <Header />
-      <main style={{marginTop: 100}}>
-        <Counter/>
-      </main>
-    </div>
+    return <Router>
+      <div>
+        <Header />
+        <ul style={{marginTop: 100}}>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/counter'>Counter</Link></li>
+        </ul>
+        <hr/>
+
+        <main style={{marginTop: 100}}>
+          <Match exactly pattern='/' component={Home} />
+          <Match pattern='/login' component={Login} />
+          <Match pattern='/counter' component={Counter} />
+        </main>
+      </div>
+    </Router>
   }
 }
